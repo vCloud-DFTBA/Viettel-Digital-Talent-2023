45ab6bc5e3f892fbaaafa87247d6f5831b987cd1
hoangndst <hoangndst@outlook.com>
2023-04-26 01:43:54 +0000

    config docker and server host

--- 1. Containerization/NguyenDinhHoang/frontend/src/api/server.ts
+++ 1. Containerization/NguyenDinhHoang/frontend/src/api/server.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 // env
 
-const backendUrl = process.env.BACKEND_URL
-
+const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000"
+console.log(backendUrl)
 const server = axios.create({
   baseURL: backendUrl,
   headers: {
