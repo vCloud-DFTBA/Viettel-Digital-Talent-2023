72c9fe42240d61e38cc1f528b33d41dd0629e7c1
crvt4722 <phamvantoi2002@gmail.com>
2023-04-29 14:02:09 +0700

    First commit

---
+++ 1. Containerization/PhamVanToi/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, render_template
+import pymongo
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+def get_db():
+    client = MongoClient(host='phamvantoi',
+                         port=27017, 
+                         username='root', 
+                         password='pass',
+                        authSource="admin")
+    db = client["internees"]
+    return db
+
+
+@app.route('/')
+def get_stored_animals():
+    db=""
+    try:
+        db = get_db()
+        # _animals = db.internees.find()
+
+        # response_text = ''
+
+        # for animal in _animals:
+        #     response_text += "<li>ID: " + str(animal['id']) + ", Name: " + animal['name'] + "</li>"
+        
+
+        return render_template('index.html', value = db.internees.find())
+    except:
+        pass
+    finally:
+        if type(db)==MongoClient:
+            db.close()
+
+if __name__=='__main__':
+    # app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
