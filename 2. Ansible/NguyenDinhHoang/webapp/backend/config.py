8ddca2a11ce0fab651f58a00650e85bbc74e0236
hoangndst <hoangndst@gmail.com>
2023-05-10 13:26:24 +0000

    rm ansible

--- 2. Ansible/NguyenDinhHoang/webapp/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    MONGO_HOST = os.environ.get('MONGO_HOST')
-    MONGO_PORT = int(os.environ.get('MONGO_PORT'))
-    MONGO_USERNAME = os.environ.get('MONGO_USERNAME')
-    MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
