ae9554d753835824683ba30814710cd1605f9631
le chien <lechien.1101@gmail.com>
2023-05-13 01:36:41 +0700

    API

--- 1. Containerization/Le Van Chien/app/app.py
+++ 1. Containerization/Le Van Chien/app/app.py
@@ -1,22 +1,29 @@
 from flask import Flask
-from flask import render_template
 from pymongo import MongoClient
+from flask_cors import CORS, cross_origin
 import json
+from bson import json_util
 
 app = Flask(__name__)
-print('start')
-@app.route('/')
-def hello():
-    client = MongoClient('mongodb://db:27017')
-    # client.drop_database('attendee')
-    db = client.attendee
-    if 'attendees' not in db.list_collection_names():
-        with open('data/attendees.json') as f:
-            data = json.load(f)
-        db.attendees.insert_many(data)
-    
-    attendees = list(db.attendees.find({}))
-    
-    return render_template('index.html', attendees=attendees)
+cors = CORS(app)
+
+client = MongoClient('mongodb://db:27017')
+client.drop_database('attendee')
+db = client.attendee
+if 'attendees' not in db.list_collection_names():
+    with open('data/attendees.json') as f:
+        data = json.load(f)
+    db.attendees.insert_many(data)
+
+@app.route('/people/', methods = ['GET'])
+def getAll():
+    attendees = json_util.dumps(db.attendees.find({}))
+    return attendees
+
+@app.route('/people/<int:num>', methods = ['GET'])
+def get(num):
+    return json_util.dumps(db.attendees.find({"no": num}))
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
