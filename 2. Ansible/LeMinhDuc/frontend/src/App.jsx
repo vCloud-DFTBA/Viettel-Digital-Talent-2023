c50a85ed22f26f49e81b59fcebaf9afc5a4bd6fb
duclm278 <palered548@gmail.com>
2023-05-13 20:21:59 +0700

    CRUD interns

--- 2. Ansible/LeMinhDuc/frontend/src/App.jsx
+++ 2. Ansible/LeMinhDuc/frontend/src/App.jsx
@@ -1,96 +1,13 @@
-import { Box, CircularProgress } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import cloudService from "./services/cloud";
+import Router from "./routes";
 
-const columns = [
-  {
-    field: "id",
-    headerName: "STT",
-    flex: 1,
-  },
-  {
-    field: "full_name",
-    headerName: "Họ và tên",
-    flex: 2,
-  },
-  {
-    field: "birth_year",
-    headerName: "Năm sinh",
-    flex: 1,
-  },
-  {
-    field: "gender",
-    headerName: "Giới tính",
-    flex: 1,
-  },
-  {
-    field: "university",
-    headerName: "Trường",
-    flex: 3,
-  },
-  {
-    field: "major",
-    headerName: "Chuyên ngành",
-    flex: 3,
-  },
-];
-
-function App() {
-  const [cloudInterns, setCloudInterns] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const tableStyle = {
-    "& .MuiDataGrid-cell:focus-within": {
-      outline: "none",
-    },
-    "& .MuiDataGrid-columnHeader:focus-within": {
-      outline: "none",
-    },
-    "& .MuiDataGrid-virtualScroller": {
-      overflow: "hidden",
-    },
-  };
-
-  useEffect(() => {
-    setLoading(true);
-    const fetchData = async () => {
-      try {
-        const interns = await cloudService.getAll();
-        setCloudInterns(interns);
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-
+export default function App() {
   return (
-    <Box className="content">
-      <h1>Viettel Digital Talent 2023</h1>
-      <h2>Cloud Interns</h2>
-
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <Box sx={{ width: "100%" }}>
-          <DataGrid
-            autoHeight
-            rows={cloudInterns}
-            columns={columns}
-            initialState={{
-              pagination: { paginationModel: { pageSize: 10 } },
-            }}
-            pageSizeOptions={[10, 20, 50]}
-            disableRowSelectionOnClick
-            sx={tableStyle}
-          />
-        </Box>
-      )}
-    </Box>
+    <BrowserRouter>
+      <CssBaseline />
+      <Router />
+    </BrowserRouter>
   );
 }
-
-export default App;
--- 2. Ansible/LeMinhDuc/frontend/src/services/cloud.js
+++ 2. Ansible/LeMinhDuc/frontend/src/services/cloud.js
@@ -7,6 +7,30 @@ const getAll = async () => {
   return response.data;
 };
 
+const getOne = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`);
+  return response.data;
+};
+
+const create = async (newObject) => {
+  const response = await axios.post(baseUrl, newObject);
+  return response.data;
+};
+
+const remove = async (id) => {
+  const response = await axios.delete(`${baseUrl}/${id}`);
+  return response.data;
+};
+
+const update = async (id, newObject) => {
+  const response = await axios.patch(`${baseUrl}/${id}`, newObject);
+  return response.data;
+};
+
 export default {
   getAll,
+  getOne,
+  create,
+  remove,
+  update,
 };
