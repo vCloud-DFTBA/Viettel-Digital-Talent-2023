2b63d8a3488f6090e110827644051dc9fa0df0ee
le chien <lechien.1101@gmail.com>
2023-05-15 19:59:00 +0700

    update

--- 1. Containerization/Le Van Chien/app/app.py
+++ 1. Containerization/Le Van Chien/app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
 import json
 from bson import json_util
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app, resources={r'/people/*': {'origins': '*'}})
 
 client = MongoClient('mongodb://db:27017')
 client.drop_database('attendee')
@@ -21,8 +21,30 @@ def getAll():
     return attendees
 
 @app.route('/people/<int:num>', methods = ['GET'])
-def get(num):
-    return json_util.dumps(db.attendees.find({"no": num}))
+def getByID(num):
+    return json_util.dumps(db.attendees.find({"id": num}))
+
+@app.route('/people/<string:username>', methods = ['GET'])
+def getByUsername(username):
+    return json_util.dumps(db.attendees.find({"username": username}))
+
+@app.route('/people/delete/<string:username>', methods = ['DELETE'])
+def delete(username):
+    db.attendees.delete_one({'username': username})
+    return json_util.dumps({'message': f'Resource with username {username} deleted'})
+
+
+@app.route('/people/create/', methods = ['PUT'])
+def create():
+    data = request.get_json()
+    db.attendees.insert_one(data)
+    return json_util.dumps({'message': 'Resource with username {} created'.format(data['username'])})
+
+@app.route('/people/update/<string:username>', methods = ['PUT'])
+def update(username):
+    data = request.get_json()
+    db.attendees.update_one({'username':username}, data)
+    return json_util.dumps({'message': 'Resource with username {} updated'.format(username)})
 
 
 if __name__ == '__main__':
