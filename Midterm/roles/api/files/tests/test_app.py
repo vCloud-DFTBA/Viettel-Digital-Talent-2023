34ce8e015b8fae4ec80d008c86e54b77b6165be8
vnMarcus <ndvinhcn@gmail.com>
2023-05-19 01:45:48 +0700

    test

--- Midterm/roles/api/files/tests/test_app.py
+++ Midterm/roles/api/files/tests/test_app.py
@@ -1,49 +1,70 @@
 import unittest
-import requests
+from pymongo import MongoClient
 
+class SetupTests(unittest.TestCase):
+    def setUp(self):
+        self.client = MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["internees"]
+        if self.db.internees.count_documents({}) == 0:
+            self.db.internees.insert_one({
+                'id': '1',
+                'name': 'Vinh dep trai 1',
+                'username': 'vinhnd',
+                'birth': '2002',
+                'sex': 'Nam',
+                'university': 'PTIT',
+                'major': 'IT'
+            })
 
-URL = "http://127.0.0.1:9090"
+    def tearDown(self):
+        self.db.internees.drop()
+        self.client.close()
 
+    def test_get_all_student(self):
+        count = self.db.internees.count_documents({})
+        self.assertEqual(count, 1)
 
-class TestApp(unittest.TestCase):
+        document = self.db.internees.find_one({'id': '1'})
+        self.assertEqual(document['name'], 'Vinh dep trai 1')
+        self.assertEqual(document['username'], 'vinhnd')
 
-    # Test web page.
-    def test_get_all_students(self):
-        response = requests.get(f"{URL}/list")
-        self.assertEqual(response.status_code, 200)
-
-    def test_insert_student(self):
-        data = {
-            'id': 37,
-            'name': 'Bùi Minh Sơn',
-            'username': 'minhson',
-            'birth': 2002,
+    def test_add_Student(self):
+        self.db.internees.insert_one({
+            'id': '2',
+            'name': 'Vinh dep trai 2',
+            'username': 'vinhnd',
+            'birth': '2002',
             'sex': 'Nam',
-            'university': 'Đại học Công nghệ - Đại học Quốc gia Hà Nội',
-            'major': 'Công nghệ thông tin'
-        }
-        response = requests.post(f"{URL}/addStudent", json=data)
-        self.assertEqual(response.status_code, 201)
+            'university': 'PTIT',
+            'major': 'IT'
+        })
+        count = self.db.internees.count_documents({})
+        self.assertEqual(count, 2)
 
-    def test_delete_student(self):
-        response = requests.delete(f"{URL}/deleteStudent/29")
-        self.assertEqual(response.status_code, 200)
+    def test_update_Student(self):
+        self.db.internees.update_one(
+            {'id': '1'},
+            {"$set": {
+                'name': 'Vinh dep trai 2',
+                'username': 'vinhnd',
+                'birth': '2002',
+                'sex': 'Nam',
+                'university': 'PTIT',
+                'major': 'IT'
+            }}
+        )
 
-    def test_get_by_id(self):
-        response = requests.get(f"{URL}/view/29")
-        self.assertEqual(response.status_code, 200)
+        document = self.db.internees.find_one({'id': '1'})
+        self.assertEqual(document['name'], 'Vinh dep trai 2')
 
-    def test_update_student(self):
-        data = {
-            'name': 'Bùi Minh Sơn',
-            'username': 'minhson',
-            'birth': 2002,
-            'sex': 'Nam',
-            'university': 'Đại học Công nghệ - Đại học Quốc gia Hà Nội',
-            'major': 'Công nghệ thông tin'
-        }
-        response = requests.put(f"{URL}/updateStudent/29", json=data)
-        self.assertEqual(response.status_code, 201)
+    def test_delete_Student(self):
+        self.db.internees.delete_one({'id': '2'})
+        count = self.db.internees.count_documents({})
+        self.assertEqual(count, 1)
 
-if __name__ == '__main__':
+    def test_view_Stuent(self):
+        student = self.db.internees.find_one({'id': '1'}, {"_id": 0})
+        self.assertEqual(student['name'], 'Vinh dep trai 1')
+
+if __name__ == "__main__":
     unittest.main()
