e9255efbbd081770018e92d38a72f5cdb2535fe4
vnMarcus <ndvinhcn@gmail.com>
2023-05-19 02:00:20 +0700

    test

--- Midterm/roles/api/files/app.py
+++ Midterm/roles/api/files/app.py
@@ -1,7 +1,6 @@
-import json
-
 from flask import Flask, jsonify, render_template, redirect, request
 from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
@@ -17,6 +16,7 @@ def index():
         api_json.append(attendee)
     return json.dumps(api_json, ensure_ascii=False).encode('utf-8')
 
+
 @app.route("/addStudent", methods=['POST'])
 def add_student():
     data = request.get_json()
@@ -30,7 +30,7 @@ def add_student():
     db.internees.insert_one({
         'id': id,
         'name': name,
-        'username' : username,
+        'username': username,
         'birth': year,
         'sex': gender,
         'university': school,
@@ -38,11 +38,13 @@ def add_student():
     })
     return jsonify({'message': 'Student added successfully'}), 201
 
+
 @app.route('/deleteStudent/<int:id>', methods=['DELETE'])
 def delete_student(id):
     id = str(id)
     db.internees.delete_one({'id': id})
     return jsonify({'message': 'Student deleted successfully'}), 200
+
 
 @app.route('/view/<int:id>', methods=['GET'])
 def view_student(id):
@@ -51,7 +53,7 @@ def view_student(id):
     api_json = []
     api_json.append(student)
     return json.dumps(api_json, ensure_ascii=False).encode('utf-8')
-    return jsonify({'message': 'Student not found'}), 404
+
 
 @app.route('/updateStudent/<int:id>', methods=['POST'])
 def update_student(id):
@@ -72,7 +74,7 @@ def update_student(id):
         {'id': id},
         {"$set": {
             'name': name,
-            'username' : username,
+            'username': username,
             'birth': year,
             'sex': gender,
             'university': school,
@@ -80,6 +82,7 @@ def update_student(id):
         }}
     )
     return jsonify({'message': 'Student updated successfully'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
