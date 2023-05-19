1eba8c3b3650f42afbc279a03858b0cdf32e09ff
letrongminh <kimchon999555@gmail.com>
2023-05-19 11:12:40 +0300

    ci

--- 1. Containerization/Le-Trong-Minh/backend/api/test_api.py
+++ 1. Containerization/Le-Trong-Minh/backend/api/test_api.py
@@ -1,9 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
 from main import app
-import logging
 
 client = TestClient(app)
+
 @pytest.fixture(scope="module")
 def student_data():
     return {
@@ -16,13 +16,13 @@ def student_data():
         "major": "CS"
     }
 
-def get_all():
+
+def test_get_all():
     response = client.get("/api/v1/students")
     assert response.status_code == 200
     assert response.json() is not None
 
-
-def post_(student_data):
+def test_post(student_data):
     response = client.post("/api/v1/students", json=student_data)
     assert response.status_code == 200
     response_data = response.json()
@@ -42,7 +42,8 @@ def post_(student_data):
     }
     assert response_data == expected_data
 
-def get_by_id(student_data):
+
+def test_get_by_id(student_data):
     response1 = client.post("/api/v1/students", json=student_data)
     student_id = response1.json()['data']['id']
     response2 = client.get(f"/api/v1/students/{student_id}")
@@ -64,7 +65,7 @@ def get_by_id(student_data):
     }
     assert response2_data == expected_data
 
-def update_(student_data):
+def test_update(student_data):
     response1 = client.post("/api/v1/students", json=student_data)
     student_id = response1.json()['data']['id']
     updated_data = {
@@ -85,7 +86,7 @@ def update_(student_data):
     }
     assert response2.json() == expected_data
 
-def delete_(student_data):
+def test_delete(student_data):
     response1 = client.post("/api/v1/students", json=student_data)
     student_id = response1.json()['data']['id']
     response2 = client.delete(f"/api/v1/students/{student_id}")
