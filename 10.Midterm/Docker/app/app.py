c5928b1973abea37700ee69a9fa31df9d932eb7a
ubuntu1 <phuclaile2708@gmail.com>
2023-05-20 20:59:10 +0700

    midterm

---
+++ 10.Midterm/Docker/app/app.py
@@ -0,0 +1,59 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson import ObjectId 
+
+application = Flask(__name__, template_folder='template')
+
+application.config["MONGO_URI"] = "mongodb://host_mongodb:27017/VDT23"
+mongo = PyMongo(application)
+db = mongo.db
+
+# def redirect_url():
+#     return request.args.get('next') or \
+#            request.referrer or \
+#            url_for('index')
+
+@application.route("/")
+def lists ():
+	curs = db.attendees.find()
+	a1="active"
+	return render_template('index.html',a1=a1,curs=curs)
+
+@application.route("/action", methods=['POST'])
+def action ():
+	Name=request.values.get("Name")
+	DOB=request.values.get("DOB")
+	Gender=request.values.get("Gender")
+	University=request.values.get("University")
+	Major=request.values.get("Major")
+	Username=request.values.get("Username")
+	db.attendees.insert_one({ "Name":Name, "DOB":DOB, "Gender":Gender, "University":University, "Major":Major, "Username": Username})
+	return redirect("/")
+
+@application.route("/remove")
+def remove ():
+	key=request.values.get("_id")
+	db.attendees.delete_one({"_id":ObjectId(key)})
+	return redirect("/")
+
+@application.route("/update")
+def update ():
+	key=request.values.get("_id")
+	curs=db.attendees.find({"_id":ObjectId(key)})
+	return render_template('update.html',curs=curs)
+
+@application.route("/action3", methods=['POST'])
+def action3 ():
+	Name=request.values.get("Name")
+	DOB=request.values.get("DOB")
+	Gender=request.values.get("Gender")
+	University=request.values.get("University")
+	Major=request.values.get("Major")
+	Username=request.values.get("Username")
+	key=request.values.get("_id")
+	db.attendees.update_one({"_id":ObjectId(key)}, {'$set':{"Name":Name, "DOB":DOB, "Gender":Gender, "University":University, "Major":Major, "Username":Username}})
+	return redirect("/")
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000, debug=True)
---
+++ 10.Midterm/Docker/app/test.py
@@ -0,0 +1,75 @@
+import unittest
+from bson import ObjectId
+from app import application, db
+
+class FlaskAppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        application.config["TESTING"] = True
+        self.app = application.test_client()
+
+    def test_lists_route(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_action_route(self):
+        data = {
+            "Name": "Test",
+            "DOB": "2000",
+            "Gender": "Name",
+            "University": "HUST",
+            "Major": "ET",
+            "Username": "test"
+        }
+        response = self.app.post("/action", data=data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_remove_route(self):
+        test_doc = db.attendees.insert_one({
+            "Name": "Test",
+            "DOB": "2000",
+            "Gender": "Name",
+            "University": "HUST",
+            "Major": "ET",
+            "Username": "test"
+        })
+        response = self.app.get(f"/remove?key={test_doc.inserted_id}")
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_route(self):
+        test_doc = db.attendees.insert_one({
+            "Name": "Test",
+            "DOB": "2000",
+            "Gender": "Name",
+            "University": "HUST",
+            "Major": "ET",
+            "Username": "test"
+        })
+        response = self.app.get(f"/update?key={test_doc.inserted_id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_action3_route(self):
+        test_doc = db.attendees.insert_one({
+            "Name": "Test",
+            "DOB": "2000",
+            "Gender": "Name",
+            "University": "HUST",
+            "Major": "ET",
+            "Username": "test"
+        })
+        data = {
+            "_id": str(test_doc.inserted_id),
+            "Name": "Updated Test",
+            "DOB": "2000",
+            "Gender": "Name",
+            "University": "HUST",
+            "Major": "ET",
+            "Username": "test"
+        }
+        response = self.app.post("/action3", data=data)
+        self.assertEqual(response.status_code, 302)
+        updated_doc = db.attendees.find_one({"_id": ObjectId(test_doc.inserted_id)})
+        self.assertEqual(updated_doc["Name"], "Updated Test")
+
+if __name__ == "__main__":
+    unittest.main()
