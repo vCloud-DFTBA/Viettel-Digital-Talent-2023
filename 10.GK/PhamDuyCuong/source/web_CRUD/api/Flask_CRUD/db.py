a239057c2fe79e15431e9b04046bb377e8623b7f
pdcuong <pdcuong11@gmail.com>
2023-05-22 02:15:49 -0400

    test ansible

--- 10.GK/PhamDuyCuong/source/web_CRUD/api/Flask_CRUD/db.py
+++ 10.GK/PhamDuyCuong/source/web_CRUD/api/Flask_CRUD/db.py
@@ -4,19 +4,14 @@ from pymongo import MongoClient
 
 
 def init_db(path_to_csv):
-    DB_HOSTNAME="44.201.231.16"
-    DB_USERNAME="admin"
-    DB_PASSWORD="vdt23"
+    DB_HOSTNAME="ec2-44-201-231-16.compute-1.amazonaws.com:27017/"
     DATABASE_NAME="VDT2023"
     logger = logging.getLogger()
     try:
-        MONGO_URI =f"mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:27017"
-        print(MONGO_URI)
+        MONGO_URI =f"mongodb://{DB_HOSTNAME}"
         client = MongoClient(MONGO_URI)
         db = client[DATABASE_NAME]
         collection = db.attendees
-        logger.warning("Conected to database")
-
         if collection.estimated_document_count() == 0:
             rows = []
             csvFile = open(path_to_csv, encoding="utf-8")
@@ -24,6 +19,7 @@ def init_db(path_to_csv):
             for row in csv_reader:
                 rows.append(row)
             collection.insert_many(rows)
+        logger.warning("Conected to database")
         return collection
     except BaseException:
         logger = logging.getLogger()
