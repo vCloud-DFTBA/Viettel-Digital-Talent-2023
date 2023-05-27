e54c1ece5699a147f26f179ad9ee7eeaf10cee4a
nguyenthangg <108355000+nguyenthangg@users.noreply.github.com>
2023-05-27 15:48:49 +0700

    midterm1

---
+++ GiuaKi_nguyenhuuthang/apps/data/createtestdata.py
@@ -0,0 +1,27 @@
+import json
+import pymongo 
+  
+  
+# Making Connection
+myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/") 
+   
+# database 
+db = myclient["datatest"]
+
+
+# Created or Switched to collection 
+
+Collection = db["studenttest"]
+
+# Loading or Opening the json file
+with open('student.json') as file:
+    file_data = json.load(file)
+      
+# Inserting the loaded data in the Collection
+# if JSON contains data more than one entry
+# insert_many is used else inser_one is used
+if isinstance(file_data, list):
+    Collection.insert_many(file_data)  
+else:
+    Collection.insert_one(file_data)
+
