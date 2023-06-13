04aa1e7086be95a611feefe5b4d3870d7e0b8e5c
blackcat4347 <chihuong195195@gmail.com>
2023-06-13 19:19:03 +0700

    remove

--- 1. Containerization/NinhChiHuong/backend/database/models.py
+++
@@ -1,8 +0,0 @@
-from .db import db
-
-class Student(db.Document):
-    sid = db.IntField(unique=True)
-    full_name = db.StringField(max_length=50)
-    year_of_birth = db.IntField()
-    university = db.StringField(max_length=60)
-    major = db.StringField(max_length=60)
